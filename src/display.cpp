battle-for-wesnoth/svn

commit 33fd019ad4c4983941d8abccea62902ec093112f

remove the darkened image cache, overlay images/terrain/darken.png instead. this should reduce mem consumption a lot... but there is still work to do. Artists should feel free to imprve the new image
git-svn-id: svn+ssh://svn.gna.org/svn/wesnoth/trunk@14361 75b3775d-3801-0410-9e12-e2e43121521d

src/display.cpp
@@ -1386,15 +1386,6 @@ void display::draw_tile(const gamemap::location &loc, const SDL_Rect &clip_rect)
 		mask = tod_at.image_mask;
 	}
 
-	//find if this tile should be darkened or bightened (reach of a unit)
-	if (!reach_map_.empty()) {
-		reach = reach_map_.find(loc);
-		if (reach == reach_map_.end()) {
-			image_type = image::DARKENED;
-		} else {
-			image_type = image::UNMASKED;
-		}
-	}
 	unit_map::iterator un = find_visible_unit(units_, loc, map_,
 											  teams_,teams_[currentTeam_]);
 
@@ -1480,6 +1471,17 @@ void display::draw_tile(const gamemap::location &loc, const SDL_Rect &clip_rect)
 		if(img != NULL) {
 			SDL_Rect dstrect = { xpos, ypos, 0, 0 };
 			SDL_BlitSurface(img,NULL,dst,&dstrect);
+		}
+	}
+	//find if this tile should be darkened or bightened (reach of a unit)
+	if (!reach_map_.empty()) {
+		reach = reach_map_.find(loc);
+		if (reach == reach_map_.end()) {
+			const surface img(image::get_image("terrain/darken.png",image::UNMASKED,image::NO_ADJUST_COLOUR));
+			if(img != NULL) {
+				SDL_Rect dstrect = { xpos, ypos, 0, 0 };
+				SDL_BlitSurface(img,NULL,dst,&dstrect);
+			}
 		}
 	}
 

src/image.cpp
@@ -40,8 +40,8 @@ typedef std::pair<image::locator::value, int> locator_finder_pair;
 locator_finder_t locator_finder;
 
 // Definition of all image maps
-image::image_cache images_,scaled_images_,unmasked_images_,greyed_images_;
-image::image_cache brightened_images_,semi_brightened_images_,darkened_images_;
+image::image_cache images_,scaled_images_,unmasked_images_;
+image::image_cache brightened_images_,semi_brightened_images_;
 
 image::locator_cache alternative_images_;
 
@@ -88,8 +88,6 @@ void flush_cache()
 	reset_cache(images_);
 	reset_cache(scaled_images_);
 	reset_cache(unmasked_images_);
-	reset_cache(greyed_images_);
-	reset_cache(darkened_images_);
 	reset_cache(brightened_images_);
 	reset_cache(semi_brightened_images_);
 	reset_cache(alternative_images_);
@@ -110,8 +108,6 @@ void locator::init_index()
 		images_.push_back(cache_item<surface>());
 		scaled_images_.push_back(cache_item<surface>());
 		unmasked_images_.push_back(cache_item<surface>());
-		greyed_images_.push_back(cache_item<surface>());
-		darkened_images_.push_back(cache_item<surface>());
 		brightened_images_.push_back(cache_item<surface>());
 		semi_brightened_images_.push_back(cache_item<surface>());
 
@@ -488,8 +484,6 @@ void set_colour_adjustment(int r, int g, int b)
 		green_adjust = g;
 		blue_adjust = b;
 		reset_cache(scaled_images_);
-		reset_cache(greyed_images_);
-		reset_cache(darkened_images_);
 		reset_cache(brightened_images_);
 		reset_cache(semi_brightened_images_);
 		reset_cache(alternative_images_);
@@ -519,8 +513,6 @@ void set_image_mask(const std::string& image)
 	if(image_mask != image) {
 		image_mask = image;
 		reset_cache(scaled_images_);
-		reset_cache(greyed_images_);
-		reset_cache(darkened_images_);
 		reset_cache(brightened_images_);
 		reset_cache(semi_brightened_images_);
 		reset_cache(alternative_images_);
@@ -534,8 +526,6 @@ void set_zoom(int amount)
 	if(amount != zoom) {
 		zoom = amount;
 		reset_cache(scaled_images_);
-		reset_cache(greyed_images_);
-		reset_cache(darkened_images_);
 		reset_cache(brightened_images_);
 		reset_cache(semi_brightened_images_);
 		reset_cache(unmasked_images_);
@@ -584,19 +574,6 @@ surface get_scaled(const locator i_locator, COLOUR_ADJUSTMENT adj)
 	return res;
 }
 
-surface get_greyed(const locator i_locator, COLOUR_ADJUSTMENT adj)
-{
-	surface image(get_image(i_locator, SCALED, adj));
-
-	return surface(greyscale_image(image));
-}
-
-surface get_darkened(const locator i_locator, COLOUR_ADJUSTMENT adj)
-{
-	surface image(get_image(i_locator, SCALED, adj));
-
-	return surface(darken_image(image));
-}
 
 surface get_brightened(const locator i_locator, COLOUR_ADJUSTMENT adj)
 {
@@ -627,12 +604,6 @@ surface get_image(const image::locator& i_locator, TYPE type, COLOUR_ADJUSTMENT 
 		break;
 	case UNMASKED:
 		imap = &unmasked_images_;
-		break;
-	case GREYED:
-		imap = &greyed_images_;
-		break;
-	case DARKENED:
-		imap = &darkened_images_;
 		break;
 	case BRIGHTENED:
 		imap = &brightened_images_;
@@ -666,12 +637,6 @@ surface get_image(const image::locator& i_locator, TYPE type, COLOUR_ADJUSTMENT 
 			break;
 		case UNMASKED:
 			res = get_unmasked(i_locator);
-			break;
-		case GREYED:
-			res = get_greyed(i_locator, adj);
-			break;
-		case DARKENED:
-			res = get_darkened(i_locator, adj);
 			break;
 		case BRIGHTENED:
 			res = get_brightened(i_locator, adj);

src/image.hpp
@@ -28,7 +28,6 @@
 /// - unscaled: no modifications have been done on the image.
 /// - scaled: images are scaled to the size of a tile
 /// - unmasked: images are scaled, but have no time of day masking applied to them
-/// - greyed: images are scaled and in greyscale
 /// - brightened: images are scaled and brighter than normal.
 namespace image {
 	template<typename T>
@@ -174,7 +173,7 @@ namespace image {
 
 	// unscaled : image will be drawn "as is" without changing size, even in case of redraw
 	// scaled : image will be scaled to fit into a hex, taking zoom into account
-	enum TYPE { UNSCALED, SCALED, UNMASKED, GREYED, DARKENED, BRIGHTENED, SEMI_BRIGHTENED };
+	enum TYPE { UNSCALED, SCALED, UNMASKED,  BRIGHTENED, SEMI_BRIGHTENED };
 
 	enum COLOUR_ADJUSTMENT { ADJUST_COLOUR, NO_ADJUST_COLOUR };
 

src/unit.cpp
@@ -32,6 +32,7 @@
 #include "actions.hpp"
 #include "game_events.hpp"
 #include "sound.hpp"
+#include "sdl_utils.hpp"
 
 #include <ctime>
 #include <algorithm>
@@ -1949,7 +1950,7 @@ void unit::redraw_unit(display& disp,gamemap::location hex)
 #endif
 
 	surface image(image::get_image(loc,
-				utils::string_bool(get_state("stoned"))?image::GREYED : image::UNSCALED,image::ADJUST_COLOUR,
+				image::UNSCALED,image::ADJUST_COLOUR,
 #ifndef LOW_MEM
 				true));
 #else
@@ -1958,6 +1959,9 @@ void unit::redraw_unit(display& disp,gamemap::location hex)
 
 	if(image == NULL) {
 		image = still_image();
+	}
+	if(utils::string_bool(get_state("stoned"))) {
+		image = greyscale_image(image);
 	}
 
 	if(facing_ == gamemap::location::NORTH_WEST || facing_ == gamemap::location::SOUTH_WEST) {

commit 0965e520e24775efb7b9778b064bb2304a9fdfc6

"show enemy moves" shows numbers again if multiple units can reach a location this was a regression in r14361 (removing darkened image cache)
git-svn-id: svn+ssh://svn.gna.org/svn/wesnoth/trunk@15789 75b3775d-3801-0410-9e12-e2e43121521d

src/display.cpp
@@ -1352,8 +1352,6 @@ void display::draw_terrain_on_tile(int x, int y, image::TYPE image_type, ADJACEN
 
 void display::draw_tile(const gamemap::location &loc, const SDL_Rect &clip_rect)
 {
-	reach_map::iterator reach = reach_map_.end();
-
 	if(screen_.update_locked()) {
 		return;
 	}
@@ -1451,8 +1449,6 @@ void display::draw_tile(const gamemap::location &loc, const SDL_Rect &clip_rect)
 
 	if(!shrouded(loc.x,loc.y)) {
 		draw_terrain_on_tile(loc.x,loc.y,image_type,ADJACENT_FOGSHROUD);
-		if (reach != reach_map_.end())
-			draw_enemies_reach(reach->second,xpos,ypos);
 	}
 
 	//draw the time-of-day mask on top of the hex
@@ -1475,13 +1471,15 @@ void display::draw_tile(const gamemap::location &loc, const SDL_Rect &clip_rect)
 	}
 	//find if this tile should be darkened or bightened (reach of a unit)
 	if (!reach_map_.empty()) {
-		reach = reach_map_.find(loc);
+		reach_map::iterator reach = reach_map_.find(loc);
 		if (reach == reach_map_.end()) {
 			const surface img(image::get_image("terrain/darken.png",image::UNMASKED,image::NO_ADJUST_COLOUR));
 			if(img != NULL) {
 				SDL_Rect dstrect = { xpos, ypos, 0, 0 };
 				SDL_BlitSurface(img,NULL,dst,&dstrect);
 			}
+		} else {
+			draw_enemies_reach(reach->second,xpos,ypos);
 		}
 	}
 