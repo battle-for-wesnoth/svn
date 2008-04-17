battle-for-wesnoth/svn

commit bd7c61d1cdc6b374d146d417f174aa31063f033d

Remove bad code.  This fixes bug #11451.
git-svn-id: svn+ssh://svn.gna.org/svn/wesnoth/trunk@25670 75b3775d-3801-0410-9e12-e2e43121521d

src/unit.cpp
@@ -1264,15 +1264,6 @@ void unit::read(const config& cfg, bool use_traits, game_state* state)
 
 	type_ = "";
 	advance_to(&uti->second.get_gender_unit_type(gender_), use_traits, state);
-		attacks_left_ = max_attacks_;
-		if(cfg["moves"]=="") {
-			movement_ = max_movement_;
-		}
-		if(movement_ < 0) {
-			attacks_left_ = 0;
-			movement_ = 0;
-		}
-
 	type_ = cfg_["type"];
 	if(cfg["race"] != "") {
 		const race_map::const_iterator race_it = unit_type_data::types().races().find(cfg["race"]);

commit bc3213b71703d05e38cf2071f057c204bbb2a711

fix a bug introduced in r25670: units can not attack at the first turn This restore the previous behavior, give all its attacks to an unit when key attack_left is missing. Seems not to resurrect bug #11451. Still another strange thing: at the first turn, we probably must call new_turn() for every units not only the leader (it's also an alternative fix)
git-svn-id: svn+ssh://svn.gna.org/svn/wesnoth/trunk@25893 75b3775d-3801-0410-9e12-e2e43121521d

src/unit.cpp
@@ -1264,7 +1264,7 @@ void unit::read(const config& cfg, bool use_traits, game_state* state)
 	if(recruits_.size() == 1 && recruits_.front() == "") {
 		recruits_.clear();
 	}
-	attacks_left_ = lexical_cast_default<int>(cfg["attacks_left"]);
+
 	const config* mods = cfg.child("modifications");
 	if(mods) {
 		modifications_ = *mods;
@@ -1282,9 +1282,12 @@ void unit::read(const config& cfg, bool use_traits, game_state* state)
 		}
 	}
 	variation_ = cfg["variation"];
+
 	if(cfg["max_attacks"] != "") {
 		max_attacks_ = minimum<int>(1,lexical_cast_default<int>(cfg["max_attacks"]));
 	}
+	attacks_left_ = lexical_cast_default<int>(cfg["attacks_left"], max_attacks_);
+
 	if(cfg["zoc"] != "") {
 		emit_zoc_ = lexical_cast_default<int>(cfg["zoc"]);
 	}