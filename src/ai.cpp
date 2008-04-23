battle-for-wesnoth/svn

commit b4fafa21495b9e90428186b0592771167b371015

merged in formula AI branch
git-svn-id: svn+ssh://svn.gna.org/svn/wesnoth/trunk@24256 75b3775d-3801-0410-9e12-e2e43121521d

src/ai.cpp
@@ -1,6 +1,6 @@
 /* $Id$ */
 /*
-   Copyright (C) 2003 - 2008 by David White <dave@whitevine.net>
+   Copyright (C) 2003 - 2007 by David White <dave@whitevine.net>
    Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
@@ -24,7 +24,10 @@
 #include "ai_python.hpp"
 #endif
 #include "actions.hpp"
+#include "callable_objects.hpp"
 #include "dialogs.hpp"
+#include "foreach.hpp"
+#include "formula_ai.hpp"
 #include "game_config.hpp"
 #include "game_events.hpp"
 #include "game_preferences.hpp"
@@ -34,6 +37,7 @@
 #include "replay.hpp"
 #include "statistics.hpp"
 #include "unit_display.hpp"
+#include "unit.hpp"
 #include "playturn.hpp"
 #include "wml_exception.hpp"
 
@@ -202,6 +206,8 @@ ai_interface* create_ai(const std::string& name, ai_interface::info& info)
 		return new sample_ai(info);
 	else if(name == "idle_ai")
 		return new idle_ai(info);
+	else if(name == "formula_ai")
+		return new formula_ai(info);
 	else if(name == "dfool_ai")
 		return new dfool::dfool_ai(info);
 	//else if(name == "advanced_ai")
@@ -416,8 +422,7 @@ gamemap::location ai_interface::move_unit_partial(location from, location to,
 	// Stop the user from issuing any commands while the unit is moving.
 	const events::command_disabler disable_commands;
 
-	// show the unit in the sidebar without highlighting it
-	info_.disp.display_unit_hex(from);
+	info_.disp.select_hex(from);
 	info_.disp.update_display();
 
 	log_scope2(ai, "move_unit");
@@ -434,6 +439,10 @@ gamemap::location ai_interface::move_unit_partial(location from, location to,
 	}
 
 	const bool show_move = preferences::show_ai_moves();
+
+	const bool teleport = u_it->second.get_ability_bool("teleport",u_it->first);
+	paths current_paths(info_.map,info_.units,from,
+			info_.teams,false,teleport,current_team());
 
 	const std::map<location,paths>::iterator p_it = possible_moves.find(from);
 
@@ -517,6 +526,8 @@ gamemap::location ai_interface::move_unit_partial(location from, location to,
 
 			if(show_move && unit_display::unit_visible_on_path(steps,
 						u_it->second, info_.units,info_.teams)) {
+
+				info_.disp.scroll_to_tiles(from,to);
 
 				unit_map::iterator up = info_.units.find(u_it->first);
 				unit_display::move_unit(steps,up->second,info_.teams);
@@ -678,7 +689,7 @@ void ai::attack_enemy(const location& attacking_unit, const location& target,
 
 void ai_interface::calculate_possible_moves(std::map<location,paths>& res, move_map& srcdst,
 		move_map& dstsrc, bool enemy, bool assume_full_movement,
-		const std::set<gamemap::location>* remove_destinations)
+		const std::set<gamemap::location>* remove_destinations) const
 {
   calculate_moves(info_.units,res,srcdst,dstsrc,enemy,assume_full_movement,remove_destinations);
 }
@@ -687,7 +698,7 @@ void ai_interface::calculate_moves(const unit_map& units, std::map<location,path
 		move_map& dstsrc, bool enemy, bool assume_full_movement,
 	     const std::set<gamemap::location>* remove_destinations,
 		bool see_all
-          )
+          ) const
 {
 
 	for(unit_map::const_iterator un_it = units.begin(); un_it != units.end(); ++un_it) {
@@ -1109,10 +1120,8 @@ void ai_interface::attack_enemy(const location u,
 			LOG_STREAM(err, ai) << "attempt to attack twice with the same unit\n";
 			return;
 		}
-		if(weapon >= 0)
-		{
-			recorder.add_attack(u,target,weapon,def_weapon);
-		}
+
+		recorder.add_attack(u,target,weapon,def_weapon);
 		try {
 			attack(info_.disp, info_.map, info_.teams, u, target, weapon, def_weapon,
 					info_.units, info_.state, info_.gameinfo);
@@ -2173,4 +2182,148 @@ int ai::attack_depth()
 	const config& parms = current_team().ai_parameters();
 	attack_depth_ = maximum<int>(1,lexical_cast_default<int>(parms["attack_depth"],5));
 	return attack_depth_;
+}
+
+namespace {
+template<typename Container>
+variant villages_from_set(const Container& villages,
+				          const std::set<gamemap::location>* exclude=NULL) {
+	std::vector<variant> vars;
+	foreach(const gamemap::location& loc, villages) {
+		if(exclude && exclude->count(loc)) {
+			continue;
+		}
+		vars.push_back(variant(new location_callable(loc)));
+	}
+
+	return variant(&vars);
+}
+}
+
+variant ai_interface::get_value(const std::string& key) const
+{
+	if(key == "turn") {
+		return variant(get_info().state.turn());
+	} else if(key == "units") {
+		std::vector<variant> vars;
+		for(unit_map::const_iterator i = info_.units.begin(); i != info_.units.end(); ++i) {
+			vars.push_back(variant(new unit_callable(*i, info_.teams[info_.team_num-1], info_.team_num)));
+		}
+		return variant(&vars);
+	} else if(key == "my_units") {
+		std::vector<variant> vars;
+		for(unit_map::const_iterator i = info_.units.begin(); i != info_.units.end(); ++i) {
+			if(i->second.side() == info_.team_num) {
+				vars.push_back(variant(new unit_callable(*i, info_.teams[info_.team_num-1], info_.team_num)));
+			}
+		}
+		return variant(&vars);
+	} else if(key == "enemy_units") {
+		std::vector<variant> vars;
+		for(unit_map::const_iterator i = info_.units.begin(); i != info_.units.end(); ++i) {
+			if(info_.teams[info_.team_num-1].is_enemy(i->second.side())) {
+				vars.push_back(variant(new unit_callable(*i, info_.teams[info_.team_num-1], info_.team_num)));
+			}
+		}
+		return variant(&vars);
+	} else if(key == "villages") {
+		return villages_from_set(info_.map.villages());
+	} else if(key == "my_villages") {
+		return villages_from_set(current_team().villages());
+	} else if(key == "enemy_and_unowned_villages") {
+		return villages_from_set(info_.map.villages(), &current_team().villages());
+	} else if(key == "map") {
+		return variant(new gamemap_callable(info_.map));
+	}
+	return variant();
+}
+
+void ai_interface::get_inputs(std::vector<game_logic::formula_input>* inputs) const
+{
+	using game_logic::FORMULA_READ_ONLY;
+	inputs->push_back(game_logic::formula_input("turn", FORMULA_READ_ONLY));
+	inputs->push_back(game_logic::formula_input("units", FORMULA_READ_ONLY));
+	inputs->push_back(game_logic::formula_input("my_units", FORMULA_READ_ONLY));
+	inputs->push_back(game_logic::formula_input("enemy_units", FORMULA_READ_ONLY));
+	inputs->push_back(game_logic::formula_input("villages", FORMULA_READ_ONLY));
+	inputs->push_back(game_logic::formula_input("my_villages", FORMULA_READ_ONLY));
+	inputs->push_back(game_logic::formula_input("enemy_and_unowned_villages", FORMULA_READ_ONLY));
+	inputs->push_back(game_logic::formula_input("map", FORMULA_READ_ONLY));
+}
+
+variant ai::attack_analysis::get_value(const std::string& key) const
+{
+	using namespace game_logic;
+	if(key == "target") {
+		return variant(new location_callable(target));
+	} else if(key == "movements") {
+		std::vector<variant> res;
+		for(int n = 0; n != movements.size(); ++n) {
+			map_formula_callable* item = new map_formula_callable(NULL);
+			item->add("src", variant(new location_callable(movements[n].first)));
+			item->add("dst", variant(new location_callable(movements[n].second)));
+			res.push_back(variant(item));
+		}
+
+		return variant(&res);
+	} else if(key == "units") {
+		std::vector<variant> res;
+		for(int n = 0; n != movements.size(); ++n) {
+			res.push_back(variant(new location_callable(movements[n].first)));
+		}
+
+		return variant(&res);
+	} else if(key == "target_value") {
+		return variant(static_cast<int>(target_value*1000));
+	} else if(key == "avg_losses") {
+		return variant(static_cast<int>(avg_losses*1000));
+	} else if(key == "chance_to_kill") {
+		return variant(static_cast<int>(chance_to_kill*100));
+	} else if(key == "avg_damage_inflicted") {
+		return variant(static_cast<int>(avg_damage_inflicted));
+	} else if(key == "target_starting_damage") {
+		return variant(target_starting_damage);
+	} else if(key == "avg_damage_taken") {
+		return variant(static_cast<int>(avg_damage_taken));
+	} else if(key == "resources_used") {
+		return variant(static_cast<int>(resources_used));
+	} else if(key == "terrain_quality") {
+		return variant(static_cast<int>(terrain_quality));
+	} else if(key == "alternative_terrain_quality") {
+		return variant(static_cast<int>(alternative_terrain_quality));
+	} else if(key == "vulnerability") {
+		return variant(static_cast<int>(vulnerability));
+	} else if(key == "support") {
+		return variant(static_cast<int>(support));
+	} else if(key == "leader_threat") {
+		return variant(leader_threat);
+	} else if(key == "uses_leader") {
+		return variant(uses_leader);
+	} else if(key == "is_surrounded") {
+		return variant(is_surrounded);
+	} else {
+		return variant();
+	}
+}
+
+void ai::attack_analysis::get_inputs(std::vector<game_logic::formula_input>* inputs) const
+{
+	using namespace game_logic;
+	inputs->push_back(formula_input("target", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("movements", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("units", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("target_value", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("avg_losses", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("chance_to_kill", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("avg_damage_inflicted", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("target_starting_damage", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("avg_damage_taken", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("resources_used", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("terrain_quality", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("alternative_terrain_quality", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("vulnerability", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("support", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("leader_threat", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("uses_leader", FORMULA_READ_ONLY));
+	inputs->push_back(formula_input("is_surrounded", FORMULA_READ_ONLY));
 }

data/core/hotkeys.cfg
@@ -2,6 +2,11 @@
 # Warning:
 # any hotkey that uses shift should not trigger any animation(s) in game since shift toggles accelerated mode
 
+[hotkey]
+    command=aiformula
+    key=f
+[/hotkey]
+
 #ifdef APPLE
 [hotkey]
     command=accelerated

src/Makefile.am
@@ -41,11 +41,9 @@ noinst_LIBRARIES = libwesnoth-core.a libwesnoth.a
 
 pkgdatadir=$(datadir)/@DATADIR@
 
-INTERNALLIBS = -lwesnoth-core
-
-THELIBS = -L. $(SDL_IMAGE_LIBS) $(SDL_MIXER_LIBS) $(SDL_NET_LIBS) \
+THELIBS = -L. -lwesnoth-core $(SDL_IMAGE_LIBS) $(SDL_MIXER_LIBS) $(SDL_NET_LIBS) \
 	$(SDL_TTF_LIBS) $(SDL_LIBS) $(PYTHON_LIBS) $(FREETYPE_LIBS) $(LIBINTL) \
-	$(BOOST_IOSTREAMS_LIBS) 
+	$(BOOST_IOSTREAMS_LIBS) $(BOOST_REGEX_LIBS)
 
 wesnoth_source = \
 	about.cpp \
@@ -58,9 +56,14 @@ wesnoth_source = \
 	ai_village.cpp \
 	animated_game.cpp \
 	attack_prediction.cpp \
+	callable_objects.cpp \
 	config_adapter.cpp \
 	dialogs.cpp \
 	floating_textbox.cpp \
+	formula.cpp \
+	formula_ai.cpp \
+	formula_function.cpp \
+	formula_tokenizer.cpp \
 	game_display.cpp \
 	game_events.cpp \
 	game_preferences.cpp \
@@ -107,8 +110,9 @@ wesnoth_source = \
 	unit_types.cpp \
 	upload_log.cpp \
 	variable.cpp \
+	variant.cpp \
 	widgets/combo.cpp \
-	widgets/scrollpane.cpp 
+	widgets/scrollpane.cpp
 
 #############################################################################
 #    Wesnoth                                                                #
@@ -118,7 +122,7 @@ wesnoth_SOURCES = \
 	game.cpp \
 	$(wesnoth_source)
 	
-wesnoth_LDADD = $(INTERNALLIBS) libwesnoth.a $(THELIBS)
+wesnoth_LDADD = $(THELIBS) libwesnoth.a
 wesnoth_DEPENDENCIES=libwesnoth-core.a libwesnoth.a
 
 #############################################################################
@@ -139,7 +143,7 @@ wesnoth_editor_SOURCES = \
 	generic_event.cpp \
 	tooltips.cpp 
 
-wesnoth_editor_LDADD = $(INTERNALLIBS) libwesnoth.a $(THELIBS)
+wesnoth_editor_LDADD = $(THELIBS) libwesnoth.a
 wesnoth_editor_DEPENDENCIES=libwesnoth-core.a libwesnoth.a
 
 #############################################################################
@@ -207,10 +211,9 @@ cutter_DEPENDENCIES=libwesnoth-core.a
 test_SOURCES = \
 	tests/main.cpp \
 	tests/test_util.cpp \
-	tests/test_network_worker.cpp \
 	$(wesnoth_source)
 
-test_LDADD = $(INTERNALLIBS) $(BOOST_UNIT_TEST_FRAMEWORK_LIBS) libwesnoth.a $(THELIBS)  
+test_LDADD = $(THELIBS) $(BOOST_UNIT_TEST_FRAMEWORK_LIBS) libwesnoth.a 
 test_DEPENDENCIES=libwesnoth-core.a libwesnoth.a
 
 #############################################################################
@@ -480,10 +483,6 @@ if PREFSDIR
     CXXFLAGS += -DPREFERENCES_DIR=\"$(prefsdir)\"
 endif
 
-##if HAVE_BUILTIN_EXPECT
-##	CXXFLAGS += -DHAVE_BUILTIN_EXPECT
-##endif
-
 if BOOST_TEST_DYN_LINK
     CXXFLAGS += -DBOOST_TEST_DYN_LINK
 endif
@@ -502,7 +501,7 @@ if STATIC
 LDFLAGS += -all-static
 endif
 
-LDFLAGS += $(BOOST_IOSTREAMS_LDFLAGS)
+LDFLAGS += $(BOOST_IOSTREAMS_LDFLAGS)  $(BOOST_REGEX_LDFLAGS)
 
 #if STATIC
 #LIBS += -static -L/usr/lib -Wl,-rpath,/usr/lib -L/usr/X11R6/lib -lSDL_image \

src/actions.cpp
@@ -1,6 +1,6 @@
 /* $Id$ */
 /*
-   Copyright (C) 2003 - 2008 by David White <dave@whitevine.net>
+   Copyright (C) 2003 - 2007 by David White <dave@whitevine.net>
    Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
@@ -202,7 +202,7 @@ std::string recruit_unit(const gamemap& map, const int side, unit_map& units,
 
 			config cfg_unit1;
 			new_unit.write(cfg_unit1);
-			DBG_NG << cfg_unit1;
+			DBG_NG << cfg_unit1.debug();
 			if (!game_config::ignore_replay_errors) {
 				throw replay::error("OOS while recruiting.");
 			}
@@ -236,10 +236,10 @@ gamemap::location under_leadership(const unit_map& units,
 battle_context::battle_context(const gamemap& map, const std::vector<team>& teams, const unit_map& units,
 							   const gamestatus& status, const game_data& gamedata,
 							   const gamemap::location& attacker_loc, const gamemap::location& defender_loc,
-							   int attacker_weapon, int defender_weapon, double aggression, const combatant *prev_def)
+							   int attacker_weapon, int defender_weapon, double aggression, const combatant *prev_def, const unit* attacker_ptr)
 	: attacker_stats_(NULL), defender_stats_(NULL), attacker_combatant_(NULL), defender_combatant_(NULL)
 {
-	const unit& attacker = units.find(attacker_loc)->second;
+	const unit& attacker = attacker_ptr ? *attacker_ptr : units.find(attacker_loc)->second;
 	const unit& defender = units.find(defender_loc)->second;
 	const double harm_weight = 1.0 - aggression;
 
@@ -736,7 +736,7 @@ void attack::fire_event(const std::string& n)
 		dat.add_child("first");
 		dat.add_child("second");
 		if(a_ != units_.end()) {
-			(*(dat.child("first")))["weapon"]=a_stats_->weapon?a_stats_->weapon->id():"none";
+			(*(dat.child("first")))["weapon"]=a_stats_->weapon->id();
 
 		}
 		if(d_ != units_.end()) {
@@ -769,9 +769,7 @@ void attack::fire_event(const std::string& n)
 	dat.add_child("second");
 	(*(dat.child("first")))["weapon"]=a_stats_->weapon->id();
 	(*(dat.child("second")))["weapon"]=d_stats_->weapon != NULL ? d_stats_->weapon->id() : "none";
-	DELAY_END_LEVEL(delayed_exception, game_events::fire(n,
-								game_events::entity_location(attacker_,a_id_),
-								game_events::entity_location(defender_,d_id_),dat));
+	DELAY_END_LEVEL(delayed_exception, game_events::fire(n,attacker_,defender_,dat));
 	
 	// The event could have killed either the attacker or
 	// defender, so we have to make sure they still exist
@@ -802,7 +800,7 @@ void attack::refresh_bc()
 	if(a_ == units_.end() || d_ == units_.end()) {
 		return;
 	}
-	*bc_ = battle_context(map_, teams_, units_, state_, info_, attacker_, defender_, attack_with_, defend_with_);
+	*bc_ =	battle_context(map_, teams_, units_, state_, info_, attacker_, defender_, attack_with_, defend_with_);
 	a_stats_ = &bc_->get_attacker_stats();
 	d_stats_ = &bc_->get_defender_stats();
 	attacker_cth_ = a_stats_->chance_to_hit;
@@ -1046,7 +1044,7 @@ attack::attack(game_display& gui, const gamemap& map,
 				if(amount_drained > 0) {
 					char buf[50];
 					snprintf(buf,sizeof(buf),"%d",amount_drained);
-					if (update_display_ && preferences::show_combat()){
+					if (update_display_){
 						gui_.float_label(a_->first,buf,0,255,0);
 					}
 					a_->second.heal(amount_drained);
@@ -1322,11 +1320,7 @@ attack::attack(game_display& gui, const gamemap& map,
 				fire_event("attack_end");
 				DELAY_END_LEVEL(delayed_exception, game_events::fire("die",death_loc,defender_loc));
 
-				// Don't try to call refresh_bc() here the attacker or defender might have
-				// been replaced by another unit, which might have a lower number of weapons.
-				// In that case refresh_bc() will terminate with an invalid selected weapon.
-				a_ = units_.find(attacker_);
-				d_ = units_.find(defender_);
+				refresh_bc();
 
 				if(a_ == units_.end() || !death_loc.matches_unit(a_->second)) {
 					// WML has invalidated the dying unit, abort
@@ -1847,44 +1841,23 @@ bool clear_shroud_loc(const gamemap& map, team& tm,
 
 		// We clear one past the edge of the board, so that the half-hexes
 		// at the edge can also be cleared of fog/shroud.
-		if (on_board_loc || map.on_board(adj[i], true)) {
+		if (on_board_loc || map.on_board(adj[i])) {
 			// Both functions should be executed so don't use || which 
 			// uses short-cut evaluation.
 			const bool res = tm.clear_shroud(adj[i]) | tm.clear_fog(adj[i]);
 
-			if(res) {
-				result = true;
-
-				// If we're near the corner it might be the corner also needs to be cleared
-				// this always happens at the lower left corner and depending on the with
-				// at the upper or lower right corner.
-				if(adj[i].x == 0 && adj[i].y == map.h() - 1) { // Lower left corner
-					const gamemap::location corner(-1 , map.h());
-					tm.clear_shroud(corner);
-					tm.clear_fog(corner);
-
-				} else if(map.w() % 2 && adj[i].x == map.w() - 1 && adj[i].y == map.h() - 1) { // Lower right corner
-					const gamemap::location corner(map.w() , map.h());
-					tm.clear_shroud(corner);
-					tm.clear_fog(corner);
-
-				} else if(!(map.w() % 2) && adj[i].x == map.w() - 1 && adj[i].y == 0) { // Upper right corner
-					const gamemap::location corner(map.w() , -1);
-					tm.clear_shroud(corner);
-					tm.clear_fog(corner);
-				}
-
-				if(cleared) {
-					cleared->push_back(adj[i]);
-				}
+			if(res && cleared != NULL) {
+				cleared->push_back(adj[i]);
 			}
+
+			result |= res;
 		}
 	}
 
 	return result;
 }
 
-//! Returns true if some shroud is cleared.
+//! Returns true iff some shroud is cleared.
 //! seen_units will return new units that have been seen by this unit.
 //! If known_units is NULL, seen_units can be NULL and will not be changed.
 bool clear_shroud_unit(const gamemap& map,
@@ -1915,7 +1888,7 @@ bool clear_shroud_unit(const gamemap& map,
 		if(sighted != units.end() &&
 		  (sighted->second.invisible(*it,units,teams) == false
 		  || teams[team].is_enemy(sighted->second.side()) == false)) {
-			//check if we know this unit, but we always know ourself
+			//check if we know this unit, but we always know oursefl
 			//just in case we managed to move on a fogged hex (teleport)
 			if(!(seen_units == NULL || known_units == NULL)
 					&& known_units->count(*it) == 0 && *it != loc) {
@@ -1940,7 +1913,6 @@ void recalculate_fog(const gamemap& map,
 		unit_map& units,
 		std::vector<team>& teams, int team) {
 
-	assert(team >= 0 && static_cast<size_t>(team) < teams.size());
 	teams[team].refog();
 
 	for(unit_map::iterator i = units.begin(); i != units.end(); ++i) {
@@ -2168,7 +2140,6 @@ size_t move_unit(game_display* disp,
 	p->first = steps.back();
 	units.add(p);
 	ui = units.find(p->first);
-	unit::clear_status_caches();
 
 	if(move_recorder != NULL) {
 		move_recorder->add_movement(steps.front(),steps.back());

src/actions.hpp
@@ -1,6 +1,6 @@
 /* $Id$ */
 /*
-   Copyright (C) 2003 - 2008 by David White <dave@whitevine.net>
+   Copyright (C) 2003 - 2007 by David White <dave@whitevine.net>
    Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
@@ -114,7 +114,7 @@ public:
 	battle_context(const gamemap& map, const std::vector<team>& teams, const unit_map& units,
 				   const gamestatus& status, const game_data& gamedata,
 				   const gamemap::location& attacker_loc, const gamemap::location& defender_loc,
-				   int attacker_weapon = -1, int defender_weapon = -1, double aggression = 0.0, const combatant *prev_def = NULL);
+				   int attacker_weapon = -1, int defender_weapon = -1, double aggression = 0.0, const combatant *prev_def = NULL, const unit* attacker_ptr=NULL);
 
 	// Used by the AI which caches unit_stats
 	battle_context(const unit_stats &att, const unit_stats &def);

src/ai.hpp
@@ -1,6 +1,6 @@
 /* $Id$ */
 /*
-   Copyright (C) 2003 - 2008 by David White <dave@whitevine.net>
+   Copyright (C) 2003 - 2007 by David White <dave@whitevine.net>
    Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
@@ -22,6 +22,7 @@
 
 #include "actions.hpp"
 #include "ai_interface.hpp"
+#include "formula_callable.hpp"
 
 class ai : public ai_interface {
 public:
@@ -125,7 +126,8 @@ protected:
 		const location& to, const location& via,
 		const std::map<location,paths>& possible_moves) const;
 
-	struct attack_analysis
+public:
+	struct attack_analysis : public game_logic::formula_callable
 	{
 		void analyze(const gamemap& map, unit_map& units,
 					 const std::vector<team>& teams,
@@ -135,6 +137,8 @@ protected:
 					 const move_map& enemy_dstsrc, double aggression);
 
 		double rating(double aggression, class ai& ai_obj) const;
+		variant get_value(const std::string& key) const;
+		void get_inputs(std::vector<game_logic::formula_input>* inputs) const;
 
 		gamemap::location target;
 		std::vector<std::pair<gamemap::location,gamemap::location> > movements;
@@ -180,6 +184,8 @@ protected:
 		//! Is true if the units involved in this attack sequence are surrounded.
 		bool is_surrounded;
 	};
+
+protected:
 
 	virtual void do_attack_analysis(
 	                 const location& loc,

src/ai_attack.cpp
@@ -1,6 +1,6 @@
 /* $Id$ */
 /*
-   Copyright (C) 2003 - 2008 by David White <dave@whitevine.net>
+   Copyright (C) 2003 - 2007 by David White <dave@whitevine.net>
    Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
@@ -45,8 +45,10 @@ void ai::do_attack_analysis(
 	// This function is called fairly frequently, so interact with the user here.
 	raise_user_interact();
 
-	if(cur_analysis.movements.size() >= size_t(attack_depth()))
+	if(cur_analysis.movements.size() >= size_t(attack_depth())) {
+		std::cerr << "ANALYSIS " << cur_analysis.movements.size() << " >= " << attack_depth() << "\n";
 		return;
+	}
 
 	static double best_results[6];
 	if(result.empty()) {
@@ -242,6 +244,7 @@ void ai::do_attack_analysis(
 
 			cur_analysis.analyze(map_, units_, teams_, state_, gameinfo_, *this, dstsrc, srcdst, enemy_dstsrc, current_team().aggression());
 
+			//Remove this short-circuiting logic for now.. --David
 			if(cur_analysis.rating(current_team().aggression(),*this) > rating_to_beat) {
 
 				result.push_back(cur_analysis);

src/ai_interface.hpp
@@ -1,6 +1,6 @@
 /* $Id$ */
 /*
-   Copyright (C) 2003 - 2008 by David White <dave@whitevine.net>
+   Copyright (C) 2003 - 2007 by David White <dave@whitevine.net>
    Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
@@ -21,11 +21,12 @@
 class game_display;
 class gamemap;
 
+#include "formula_callable.hpp"
 #include "generic_event.hpp"
 #include "pathfind.hpp"
 #include "gamestatus.hpp"
 
-class ai_interface {
+class ai_interface : public game_logic::formula_callable {
 public:
 
 	//! A convenient typedef for the often used 'location' object
@@ -129,12 +130,12 @@ protected:
 	//!								that all units can move their full movement allotment.
 	//! 'remove_destinations':		a pointer to a set of possible destinations to omit.
 	void calculate_possible_moves(std::map<location,paths>& possible_moves, move_map& srcdst, move_map& dstsrc, bool enemy, bool assume_full_movement=false,
-	                              const std::set<location>* remove_destinations=NULL);
+	                              const std::set<location>* remove_destinations=NULL) const;
 
   //! A more fundamental version of calculate_possible_moves
   //! which allows the use of a speculative unit map.
   void calculate_moves(const unit_map& units, std::map<location,paths>& possible_moves, move_map& srcdst, move_map& dstsrc, bool enemy, bool assume_full_movement=false,
-	   const std::set<location>* remove_destinations=NULL, bool see_all=false);
+	   const std::set<location>* remove_destinations=NULL, bool see_all=false) const;
 
 	//! Recruit a unit. It will recruit the unit with the given name,
 	//! at the given location, or at an available location to recruit units
@@ -160,6 +161,9 @@ protected:
 	void raise_unit_recruited() { unit_recruited_.notify_observers(); }
 	void raise_unit_moved() {  unit_moved_.notify_observers(); }
 	void raise_enemy_attacked() { enemy_attacked_.notify_observers(); }
+protected:
+	virtual void get_inputs(std::vector<game_logic::formula_input>* inputs) const;
+	virtual variant get_value(const std::string& key) const;
 private:
 	info info_;
 	int last_interact_;

commit 3296c1eef5c1f87bbd6c1a2aeb1e5f526b22f057

Continue to fix regressions caused by r24256. Here fix r23682 and r23453 (with the change from r25992)
git-svn-id: svn+ssh://svn.gna.org/svn/wesnoth/trunk@26044 75b3775d-3801-0410-9e12-e2e43121521d

src/ai.cpp
@@ -536,8 +536,8 @@ gamemap::location ai_interface::move_unit_partial(location from, location to,
 			if(show_move && unit_display::unit_visible_on_path(steps,
 						u_it->second, info_.units,info_.teams)) {
 
-				info_.disp.select_hex(from);
-				info_.disp.scroll_to_tiles(from,to);
+				
+				info_.disp.display_unit_hex(from);
 
 				unit_map::iterator up = info_.units.find(u_it->first);
 				unit_display::move_unit(steps,up->second,info_.teams);