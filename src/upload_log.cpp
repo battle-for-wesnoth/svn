battle-for-wesnoth/svn

commit d62c8447a6320d8fc20bbadd5838b938f4dc9063

Fixed bug introduced in r37610: upload log always contains a [quit] child, even during victory and draw.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@38311 75b3775d-3801-0410-9e12-e2e43121521d

src/upload_log.cpp
@@ -349,7 +349,7 @@ bool upload_log::game_finished(config *game)
 	if (!game)
 		return false;
 
-	return game->child("victory") || game->child("defeat") || game->child("quit");
+	return game->child("victory") || game->child("defeat") || game->child("quit") || game->child("upload_log").child("ai_log");
 }
 
 config &upload_log::add_game_result(const std::string &str, int turn)

src/playsingle_controller.cpp
@@ -382,7 +382,6 @@ LEVEL_RESULT playsingle_controller::play_scenario(
 		throw;
 	} catch(end_level_exception& end_level) {
 		ai_testing::log_game_end();
-		log.quit(turn());
 		*end_level_result = end_level;
 		if(!end_level.custom_endlevel_music.empty()) {
 			switch(end_level.result) {

commit 0f3c666642092cca10a4110d38bbdfddde544737

Fixed attempted null pointer dereference that led to savegame load crashes introduced in r38311. Changed usage of child_count() in read_replay() to child().
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@38354 75b3775d-3801-0410-9e12-e2e43121521d

src/upload_log.cpp
@@ -275,7 +275,7 @@ void upload_log::read_replay()
 		if(c.child("attack")) {
 			//search through the attack to see if a unit died
 			foreach (const config &c2, c.child_range("random")) {
-				if(c2.child_count("results") && c2.child("results")["dies"] == "yes") {
+				if(c2.child("results") && c2.child("results")["dies"] == "yes") {
 					config& cfg = game_->add_child("kill_event");
 					cfg.add_child("attack",c.child("attack"));
 					cfg.add_child("results",c2.child("results"));
@@ -349,7 +349,11 @@ bool upload_log::game_finished(config *game)
 	if (!game)
 		return false;
 
-	return game->child("victory") || game->child("defeat") || game->child("quit") || game->child("upload_log").child("ai_log");
+	bool has_ai_log = false;
+	if(game->child("upload_log"))
+		has_ai_log = game->child("upload_log").child("ai_log") != 0;
+
+	return game->child("victory") || game->child("defeat") || game->child("quit") || has_ai_log;
 }
 
 config &upload_log::add_game_result(const std::string &str, int turn)