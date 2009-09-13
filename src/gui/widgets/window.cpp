battle-for-wesnoth/svn

commit 402f2b194c6b0bb865cf762ceb526205394e271c

Moved the easy close behaviour to the window.
At some places click dismiss is used which will be the new name of the
feature since it's a more fitting name.

git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@38515 75b3775d-3801-0410-9e12-e2e43121521d

src/gui/widgets/window.cpp
@@ -28,6 +28,7 @@
 #include "log.hpp"
 #include "gui/auxiliary/log.hpp"
 #include "gui/auxiliary/layout_exception.hpp"
+#include "gui/widgets/button.hpp"
 #ifdef DEBUG_WINDOW_LAYOUT_GRAPHS
 #include "gui/widgets/debug.hpp"
 #endif
@@ -662,7 +663,7 @@ void twindow::remove_linked_widget(const std::string& id
 
 void twindow::layout()
 {
-	/**** Initialize and get initial size. *****/
+	/***** Initialize. *****/
 
 	boost::intrusive_ptr<const twindow_definition::tresolution> conf =
 		boost::dynamic_pointer_cast<const twindow_definition::tresolution>
@@ -670,9 +671,6 @@ void twindow::layout()
 	assert(conf);
 
 	log_scope2(log_gui_layout, "Window: Recalculate size");
-
-	layout_init(true);
-	generate_dot_file("layout_init", LAYOUT);
 
 	const game_logic::map_formula_callable variables =
 		get_screen_size_variables();
@@ -688,6 +686,27 @@ void twindow::layout()
 				? std::min(maximum_height_, settings::screen_height)
 				: settings::screen_height
 			: h_(variables);
+
+	/***** Handle click dismiss status. *****/
+	tbutton* click_dismiss_button = NULL;
+	if((click_dismiss_button
+			= NEW_find_widget<tbutton>(this, "click_dismiss", false, false))) {
+
+		click_dismiss_button->set_visible(twidget::INVISIBLE);
+	}
+	if(easy_close_) {
+		tbutton* button = NEW_find_widget<tbutton>(this, "ok", false, false);
+		if(button) {
+			button->set_visible(twidget::INVISIBLE);
+			click_dismiss_button = button;
+		}
+		VALIDATE(click_dismiss_button
+				, _("Click dismiss needs a 'click_dismiss' or 'ok' button."));
+	}
+
+	/***** Layout. *****/
+	layout_init(true);
+	generate_dot_file("layout_init", LAYOUT);
 
 	layout_linked_widgets();
 
@@ -711,11 +730,44 @@ void twindow::layout()
 				"which doesn't fit on the screen."), sstr.str());
 	}
 
+	/****** Validate click dismiss status. *****/
+	if(easy_close_ && disable_easy_close()) {
+		assert(click_dismiss_button);
+		click_dismiss_button->set_visible(twidget::VISIBLE);
+
+
+		layout_init(true);
+		generate_dot_file("layout_init", LAYOUT);
+
+		layout_linked_widgets();
+
+		try {
+			twindow_implementation::layout(
+					*this, maximum_width, maximum_height);
+
+		} catch(tlayout_exception_resize_failed&) {
+
+			/** @todo implement the scrollbars on the window. */
+
+			std::stringstream sstr;
+			sstr << __FILE__ << ":" << __LINE__ << " in function '" << __func__
+				<< "' found the following problem: Failed to size window;"
+				<< " wanted size " << get_best_size()
+				<< " available size "
+				<< maximum_width << ',' << maximum_height
+				<< " screen size "
+				<< settings::screen_width << ',' << settings::screen_height
+				<< '.';
+
+			throw twml_exception(_("Failed to show a dialog, "
+						"which doesn't fit on the screen."), sstr.str());
+		}
+	}
+
+	/***** Get the best location for the window *****/
 	tpoint size = get_best_size();
 
 	assert(size.x <= maximum_width && size.y <= maximum_height);
-
-	/***** Get the best location for the window *****/
 
 	tpoint origin(0, 0);
 

src/gui/widgets/window.hpp
@@ -453,6 +453,15 @@ private:
 	 * be closed. The widgets in the window may override this behaviour by
 	 * registering themselves as blockers. This is tested by the function
 	 * disable_easy_close().
+	 *
+	 * The handling of easy close is done in the window, in order to do so a
+	 * window either needs a click_dismiss or an ok button. Both will be hidden
+	 * when not needed and when needed first the ok is tried and then the
+	 * click_dismiss button. this allows adding a click_dismiss button to the
+	 * window definition and use the ok from the window instance.
+	 *
+	 * @todo After testing the click dismiss feature it should be documented in
+	 * the wiki.
 	 */
 	bool easy_close_;
 

data/gui/default/widget/window_default.cfg
@@ -98,6 +98,37 @@
 
 		[/foreground]
 
+		[grid]
+
+			[row]
+
+				[column]
+
+					[grid]
+						id = "_content_grid"
+					[/grid]
+
+				[/column]
+
+			[/row]
+
+			[row]
+
+				[column]
+
+					[button]
+						id = "click_dismiss"
+						definition = "default"
+
+						label = "Close"
+					[/button]
+
+				[/column]
+
+			[/row]
+
+		[/grid]
+
 	[/resolution]
 
 [/window_definition]

src/gui/dialogs/message.cpp
@@ -100,20 +100,6 @@ void tmessage::pre_show(CVideo& /*video*/, twindow& window)
 
 	// Override the user value, to make sure it's set properly.
 	window.set_easy_close(auto_close_);
-	if(auto_close_) {
-		/*
-		 * Hide the buttton and do the layout, if window.does_easy_close() is
-		 * false the scroll_label has a scrollbar so we need to show the
-		 * button. When the button is hidden the text for the label is bigger
-		 * and thus not need a scrollbar. Also when the button is visible
-		 * easy_close will always return false.
-		 */
-		window.layout();
-
-		if(window.disable_easy_close()) {
-			set_button_visible(ok, twidget::VISIBLE);
-		}
-	}
 }
 
 void tmessage::post_show(twindow& /*window*/)

src/gui/dialogs/wml_message.cpp
@@ -196,25 +196,7 @@ void twml_message_::pre_show(CVideo& /*video*/, twindow& window)
 	} else {
 		options->set_visible(twidget::INVISIBLE);
 	}
-
-	if(!has_input_) {
-		/*
-		 * Hide the buttton and do the layout, if window.does_easy_close() is
-		 * false the scroll_label has a scrollbar so we need to show the
-		 * button. When the button is hidden the text for the label is bigger
-		 * and thus not need a scrollbar. Also when the button is visible
-		 * easy_close will always return false.
-		 */
-		tbutton* button =
-			dynamic_cast<tbutton*>(window.find_widget("ok", false));
-		VALIDATE(button, missing_widget("ok"));
-		button->set_visible(twidget::INVISIBLE);
-		window.layout();
-
-		if(window.disable_easy_close()) {
-			button->set_visible(twidget::VISIBLE);
-		}
-	}
+	window.set_easy_close(!has_input_ && option_list_.empty());
 }
 
 void twml_message_::post_show(twindow& window)

commit 1a861f43757e691c0cd86b3c13e904e273b19148

Add a helper for setting drawing_interval_.
The unit tests broke due to the fact the interval wasn't resetted which
caused the settings to have the wrong window size and crashed an
unrelated test with a resize failure. The bug was introduced in r38515.

git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@38637 75b3775d-3801-0410-9e12-e2e43121521d

src/gui/widgets/window.cpp
@@ -220,7 +220,6 @@ twindow::twindow(CVideo& video,
 	, owner_(0)
 	, need_layout_(true)
 	, suspend_drawing_(true)
-	, top_level_(false)
 	, restorer_()
 	, tooltip_()
 	, tooltip_restorer_()
@@ -361,21 +360,45 @@ twindow::tretval twindow::get_retval_by_id(const std::string& id)
 
 int twindow::show(const bool restore, const unsigned auto_close_timeout)
 {
+	/**
+	 * Helper class to set and restore the drawing interval.
+	 *
+	 * We need to make sure we restore the value when the function ends, be it
+	 * normally or due to an exception.
+	 */
+	class tdraw_interval_setter
+	{
+	public:
+		tdraw_interval_setter()
+			: interval_(draw_interval)
+		{
+			if(interval_ == 0) {
+				draw_interval = 30;
+				SDL_AddTimer(draw_interval, draw_timer, NULL);
+
+				// There might be some time between creation and showing so
+				// reupdate the sizes.
+				update_screen_size();
+
+			}
+		}
+
+		~tdraw_interval_setter()
+		{
+			draw_interval = interval_;
+		}
+	private:
+
+		int interval_;
+	};
+
 	log_scope2(log_gui_draw, "Window: show.");
 
 	generate_dot_file("show", SHOW);
 
 	assert(status_ == NEW);
 
-	top_level_ = (draw_interval == 0);
-	if(top_level_) {
-		draw_interval = 30;
-		SDL_AddTimer(draw_interval, draw_timer, NULL);
-
-		// There might be some time between creation and showing so reupdate
-		// the sizes.
-		update_screen_size();
-	}
+	tdraw_interval_setter draw_interval_setter;
 
 	/*
 	 * Before show has been called, some functions might have done some testing
@@ -417,10 +440,6 @@ int twindow::show(const bool restore, const unsigned auto_close_timeout)
 	}
 
 	suspend_drawing_ = true;
-
-	if(top_level_) {
-		draw_interval = 0;
-	}
 
 	// restore area
 	if(restore) {

src/gui/widgets/window.hpp
@@ -387,14 +387,6 @@ private:
 	/** Avoid drawing the window.  */
 	bool suspend_drawing_;
 
-	/**
-	 * The first window shown is the toplevel window.
-	 *
-	 * The toplevel window is the one that starts and stops the drawing timer.
-	 * It's set when the timer is 0 when the window is shown.
-	 */
-	bool top_level_;
-
 	/** When the window closes this surface is used to undraw the window. */
 	surface restorer_;
 