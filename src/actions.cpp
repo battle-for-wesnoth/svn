battle-for-wesnoth/svn

commit 830c7637b71a39e0c021e90a3a61db3e39d9bc79

Fixed missing code for the liminal alignment.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@45105 75b3775d-3801-0410-9e12-e2e43121521d

src/actions.cpp
@@ -1986,12 +1986,26 @@ int combat_modifier(const map_location &loc,
 {
 	const time_of_day &tod = resources::tod_manager->time_of_day_at(loc);
 
-	int bonus = tod.lawful_bonus;
+	int bonus;
+	int lawful_bonus = tod.lawful_bonus;
+	int liminal_bonus = tod.liminal_bonus;
 
-	if(alignment == unit_type::NEUTRAL)
-		bonus = 0;
-	else if(alignment == unit_type::CHAOTIC)
-		bonus = -bonus;
+	switch(alignment) {
+		case unit_type::LAWFUL:
+			bonus = lawful_bonus;
+			break;
+		case unit_type::NEUTRAL:
+			bonus = 0;
+			break;
+		case unit_type::CHAOTIC:
+			bonus = -lawful_bonus;
+			break;
+		case unit_type::LIMINAL:
+			bonus = liminal_bonus;
+		default:
+			bonus = 0;
+	}
+
 	if(is_fearless)
 		bonus = std::max<int>(bonus, 0);
 

src/menu_events.cpp
@@ -3425,11 +3425,11 @@ void console_handler::do_unit() {
 	// But someday the code ought to be
 	// changed to allow general string
 	// alignments for UMC.
-	if (name == "alignment" && (value != "lawful" && value != "neutral" && value != "chaotic")) {
+	if (name == "alignment" && (value != "lawful" && value != "neutral" && value != "chaotic" && value != "liminal")) {
 		utils::string_map symbols;
 		symbols["alignment"] = get_arg(1);
 		command_failed(VGETTEXT("Invalid alignment: '$alignment',"
-			" needs to be one of lawful, neutral or chaotic.", symbols));
+			" needs to be one of lawful, neutral, chaotic or liminal.", symbols));
 		return;
 	}
 	if (name == "advances" ){

src/unit.cpp
@@ -475,6 +475,8 @@ unit::unit(const config &cfg, bool use_traits, game_state* state) :
 		alignment_ = unit_type::NEUTRAL;
 	} else if(align == "chaotic") {
 		alignment_ = unit_type::CHAOTIC;
+	} else if(align == "liminal") {
+		alignment_ = unit_type::LIMINAL;
 	} else if(align.empty()==false){
 		alignment_ = unit_type::NEUTRAL;
 	}
@@ -1617,6 +1619,8 @@ void unit::write(config& cfg) const
 		case unit_type::CHAOTIC:
 			cfg["alignment"] = "chaotic";
 			break;
+		case unit_type::LIMINAL:
+			cfg["alignment"] = "liminal";
 		default:
 			cfg["alignment"] = "neutral";
 	}

src/unit_types.cpp
@@ -1005,8 +1005,8 @@ int unit_type::experience_needed(bool with_acceleration) const
 
 const char* unit_type::alignment_description(unit_type::ALIGNMENT align, unit_race::GENDER gender)
 {
-	static const char* aligns[] = { N_("lawful"), N_("neutral"), N_("chaotic") };
-	static const char* aligns_female[] = { N_("female^lawful"), N_("female^neutral"), N_("female^chaotic") };
+	static const char* aligns[] = { N_("lawful"), N_("neutral"), N_("chaotic"), N_("liminal") };
+	static const char* aligns_female[] = { N_("female^lawful"), N_("female^neutral"), N_("female^chaotic"), N_("female^liminal") };
 	const char** tlist = (gender == unit_race::MALE ? aligns : aligns_female);
 
 	return (sgettext(tlist[align]));
@@ -1014,7 +1014,7 @@ const char* unit_type::alignment_description(unit_type::ALIGNMENT align, unit_ra
 
 const char* unit_type::alignment_id(unit_type::ALIGNMENT align)
 {
-	static const char* aligns[] = { "lawful", "neutral", "chaotic" };
+	static const char* aligns[] = { "lawful", "neutral", "chaotic", "liminal" };
 	return (aligns[align]);
 }
 

commit 4df248743c9b8dab5c6cf285efd0e6766cbd6a85

Add another missing break introduced in r45105.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@45107 75b3775d-3801-0410-9e12-e2e43121521d

src/actions.cpp
@@ -2002,6 +2002,7 @@ int combat_modifier(const map_location &loc,
 			break;
 		case unit_type::LIMINAL:
 			bonus = liminal_bonus;
+			break;
 		default:
 			bonus = 0;
 	}