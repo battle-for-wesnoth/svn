battle-for-wesnoth/svn

commit dafe16bac09c4ccf0cf952646f180af6e3a24cad

Fixed compilation on all Debian architectures.
The #undef was introduced in r47336, but the commit offers no reason
_why_ the macro was undefined. This patch has been tested by Rhonda on
all Debian architectures and there it caused no build problem, whereas
the r47336 caused several architectures failing to build.

The reason the issue was spotted much later is that before Debian
compiled with the ANA network. It switched to the older network code
while changing the build system from autotools to cmake, so the latter
was thought to be the cause.

Fixes Debian bug #636193 and reverts r47336.

git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@51130 75b3775d-3801-0410-9e12-e2e43121521d

src/network_worker.cpp
@@ -599,7 +599,6 @@ static SOCKET_STATE receive_buf(TCPsocket sock, std::vector<char>& buf)
 		return SOCKET_ERRORED;
 	}
 
-	#undef SDLNet_Read32
 	const int len = SDLNet_Read32(reinterpret_cast<void*>(num_buf));
 
 	if(len < 1 || len > 100000000) {

commit 6391bd6a41d6a787e4bcee92de1e59168d46ac3d

Fixed a gcc warning about strict aliasing rules breakage introduced by r51130
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@51140 75b3775d-3801-0410-9e12-e2e43121521d

src/network_worker.cpp
@@ -592,14 +592,17 @@ static SOCKET_STATE send_file(buffer* buf)
 
 static SOCKET_STATE receive_buf(TCPsocket sock, std::vector<char>& buf)
 {
-	char num_buf[4] ALIGN_4;
-	bool res = receive_with_timeout(sock,num_buf,4,false);
+	union {
+	char buf[4] ALIGN_4;
+	Uint32 num;
+	} num_buf;
+	bool res = receive_with_timeout(sock,num_buf.buf,4,false);
 
 	if(!res) {
 		return SOCKET_ERRORED;
 	}
 
-	const int len = SDLNet_Read32(reinterpret_cast<void*>(num_buf));
+	const int len = SDLNet_Read32(&num_buf);
 
 	if(len < 1 || len > 100000000) {
 		return SOCKET_ERRORED;