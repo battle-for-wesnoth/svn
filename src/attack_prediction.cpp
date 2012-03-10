battle-for-wesnoth/svn

commit 0ccca8012682abf85d3e95a95c15b1c5231780a8

Apply patch #3083 by Brilliand
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@53470 75b3775d-3801-0410-9e12-e2e43121521d

src/attack_prediction.cpp
@@ -62,11 +62,11 @@ struct prob_matrix
 
 	// A hits B.
 	void receive_blow_b(unsigned damage, unsigned slow_damage, double hit_chance,
-						bool a_slows, bool a_drains);
+						bool a_slows, int a_drain_constant, int a_drain_percent);
 
 	// B hits A.  Why can't they just get along?
 	void receive_blow_a(unsigned damage, unsigned slow_damage, double hit_chance,
-						bool b_slows, bool b_drains);
+						bool b_slows, int b_drain_constant, int b_drain_percent);
 
 	void forced_levelup_a();
 	void conditional_levelup_a();
@@ -105,17 +105,17 @@ private:
 
 	// Move this much from src to dst.  Returns true if anything transferred.
 	void xfer(unsigned dst_plane, unsigned src_plane,
-			  unsigned row_dst, unsigned col_dst,
-			  unsigned row_src, unsigned col_src,
+			  int row_dst, int col_dst,
+			  int row_src, int col_src,
 			  double prob);
 
 	// Shift columns on this plane (b taking damage).  Returns min col.
 	void shift_cols(unsigned dst, unsigned src,
-					unsigned damage, double prob, bool drain);
+					unsigned damage, double prob, int drain_constant, int drain_percent);
 
 	// Shift rows on this plane (a taking damage).  Returns new min row.
 	void shift_rows(unsigned dst, unsigned src,
-					unsigned damage, double prob, bool drain);
+					unsigned damage, double prob, int drain_constant, int drain_percent);
 
 	/** @todo FIXME: rename using _ at end. */
 	unsigned int rows_, cols_;
@@ -256,8 +256,8 @@ void prob_matrix::dump() const
 
 // xfer, shift_cols and shift_rows use up most of our time.  Careful!
 void prob_matrix::xfer(unsigned dst_plane, unsigned src_plane,
-					   unsigned row_dst, unsigned col_dst,
-					   unsigned row_src, unsigned col_src,
+					   int row_dst, int col_dst,
+					   int row_src, int col_src,
 					   double prob)
 {
 	double &src = val(src_plane, row_src, col_src);
@@ -266,10 +266,14 @@ void prob_matrix::xfer(unsigned dst_plane, unsigned src_plane,
 		src -= diff;
 
 		// This is here for drain.
-		if (col_dst >= cols_)
-			col_dst = cols_ - 1;
-		if (row_dst >= rows_)
-			row_dst = rows_ - 1;
+		if (col_dst >= (signed) cols_)
+			col_dst = (signed) cols_ - 1;
+		else if (col_dst < 0)
+			col_dst = 0;
+		if (row_dst >= (signed) rows_)
+			row_dst = (signed) rows_ - 1;
+		else if (row_dst < 0)
+			row_dst = 0;
 
 		val(dst_plane, row_dst, col_dst) += diff;
 
@@ -288,47 +292,79 @@ void prob_matrix::xfer(unsigned dst_plane, unsigned src_plane,
 }
 
 void prob_matrix::shift_cols(unsigned dst, unsigned src,
-							 unsigned damage, double prob, bool drain)
+							 unsigned damage, double prob, int drain_constant, int drain_percent)
 {
-	unsigned int row, col;
-	unsigned int shift = drain ? 1 : 31; // Avoids a branch.
+	int row, col;
+	int drainmax = (drain_percent*((signed) damage)/100+drain_constant);
+
+	if(drain_constant || drain_percent) {
+		debug(("Drains %i (%i%% of %i plus %i)\n", drainmax, drain_percent, damage, drain_constant));
+	}
 
 	if (damage >= cols_)
 		damage = cols_ - 1;
 
-	// Loop backwards so we write drain behind us, for when src == dst.
-	for (row = rows_ - 1; row > min_row_[src]; row--) {
+	// Killing blows can have strange drain amounts, so handle them first
+	for (row = min_row_[src] + 1; row < (signed) rows_; row++) {
 		// These are all going to die (move to col 0).
-		for (col = 1; col <= damage; ++col)
-			xfer(dst, src, row+(col>>shift), 0, row, col, prob);
-		for (col = damage+1; col < cols_; ++col)
-			xfer(dst, src, row+(damage>>shift), col - damage, row, col, prob);
+		for (col = 1; (unsigned) col <= damage; ++col)
+			xfer(dst, src, std::max(row+(drain_percent*col/100+drain_constant), 1), 0, row, col, prob);
+	}
+
+	// Loop downwards if we drain positive, but upwards if we drain negative,
+	// so we write behind us (for when src == dst).
+	if(drainmax > 0) {
+		for (row = rows_ - 1; row > (signed) min_row_[src]; row--) {
+			for (col = damage+1; (unsigned) col < cols_; ++col)
+				xfer(dst, src, std::max(row+drainmax, 1), col - damage, row, col, prob);
+		}
+	} else {
+		for (row = min_row_[src] + 1; (unsigned) row < rows_; row++) {
+			for (col = damage+1; (unsigned) col < cols_; ++col)
+				xfer(dst, src, std::max(row+drainmax, 1), col - damage, row, col, prob);
+		}
 	}
 }
 
 void prob_matrix::shift_rows(unsigned dst, unsigned src,
-							 unsigned damage, double prob, bool drain)
+							 unsigned damage, double prob, int drain_constant, int drain_percent)
 {
-	unsigned int row, col;
-	unsigned int shift = drain ? 1 : 31; // Avoids a branch.
+	int row, col;
+	int drainmax = (drain_percent*((signed) damage)/100+drain_constant);
+
+	if(drain_constant || drain_percent) {
+		debug(("Drains %i (%i%% of %i plus %i)\n", drainmax, drain_percent, damage, drain_constant));
+	}
 
 	if (damage >= rows_)
 		damage = rows_ - 1;
 
-	// Loop downwards so if we drain, we write behind us.
-	for (col = cols_ - 1; col > min_col_[src]; col--) {
+	// Killing blows can have strange drain amounts, so handle them first
+	for (col = min_col_[src] + 1; (unsigned) col < cols_; col++) {
 		// These are all going to die (move to row 0).
-		for (row = 1; row <= damage; ++row)
-			xfer(dst, src, 0, col+(row>>shift), row, col, prob);
-		for (row = damage+1; row < rows_; ++row)
-			xfer(dst, src, row - damage, col+(damage>>shift), row, col, prob);
+		for (row = 1; row <= (signed) damage; ++row)
+			xfer(dst, src, 0, std::max(col+(drain_percent*row/100+drain_constant), 1), row, col, prob);
+	}
+
+	// Loop downwards if we drain positive, but upwards if we drain negative,
+	// so we write behind us (for when src == dst).
+	if(drainmax > 0) {
+		for (col = cols_ - 1; col > (signed) min_col_[src]; col--) {
+			for (row = damage+1; (unsigned) row < rows_; ++row)
+				xfer(dst, src, row - damage, std::max(col+drainmax, 1), row, col, prob);
+		}
+	} else {
+		for (col = min_col_[src] + 1; (unsigned) col < cols_; col++) {
+			for (row = damage+1; (unsigned) row < rows_; ++row)
+				xfer(dst, src, row - damage, std::max(col+drainmax, 1), row, col, prob);
+		}
 	}
 }
 
 // Shift prob_matrix to reflect probability 'hit_chance'
 // that damage (up to) 'damage' is done to 'b'.
 void prob_matrix::receive_blow_b(unsigned damage, unsigned slow_damage, double hit_chance,
-								 bool a_slows, bool a_drains)
+								 bool a_slows, int a_drain_constant, int a_drain_percent)
 {
 	int src, dst;
 
@@ -351,13 +387,21 @@ void prob_matrix::receive_blow_b(unsigned damage, unsigned slow_damage, double h
 		else
 			actual_damage = damage;
 
-		shift_cols(dst, src, actual_damage, hit_chance, a_drains);
+		shift_cols(dst, src, actual_damage, hit_chance, a_drain_constant, a_drain_percent);
 		if (min_col_[src] < damage)
 			min_col_[dst] = 0;
 		else if (min_col_[src] - damage < min_col_[dst])
 			min_col_[dst] = min_col_[src] - damage;
 		if (min_row_[src] < min_row_[dst])
 			min_row_[dst] = min_row_[src];
+
+		int drain_worst = std::min<int>(a_drain_percent*((signed) actual_damage)/100+a_drain_constant, a_drain_constant);
+		if(drain_worst < 0) {
+			if((unsigned) -drain_worst >= min_row_[dst])
+				min_row_[dst] = 0;
+			else
+				min_row_[dst] += drain_worst;
+		}
 	}
 }
 
@@ -442,10 +486,10 @@ double prob_matrix::dead_prob() const
 	for (p = 0; p < 4; ++p) {
 		if (!plane_[p])
 			continue;
-		// We might count 0,0 twice, but that is always 0 anyway.
+		// Don't count 0,0 twice
 		for (row = min_row_[p]; row < rows_; ++row)
 			prob += val(p, row, 0);
-		for (col = min_col_[p]; col < cols_; ++col)
+		for (col = min_col_[p]?min_col_[p]:1; col < cols_; ++col)
 			prob += val(p, 0, col);
 	}
 	return prob;
@@ -454,7 +498,7 @@ double prob_matrix::dead_prob() const
 // Shift matrix to reflect probability 'hit_chance'
 // that damage (up to) 'damage' is done to 'a'.
 void prob_matrix::receive_blow_a(unsigned damage, unsigned slow_damage, double hit_chance,
-								 bool b_slows, bool b_drains)
+								 bool b_slows, int b_drain_constant, int b_drain_percent)
 {
 	int src, dst;
 
@@ -477,13 +521,21 @@ void prob_matrix::receive_blow_a(unsigned damage, unsigned slow_damage, double h
 		else
 			actual_damage = damage;
 
-		shift_rows(dst, src, actual_damage, hit_chance, b_drains);
+		shift_rows(dst, src, actual_damage, hit_chance, b_drain_constant, b_drain_percent);
 		if (min_row_[src] < damage)
 			min_row_[dst] = 0;
 		else if (min_row_[src] - damage < min_row_[dst])
 			min_row_[dst] = min_row_[src] - damage;
 		if (min_col_[src] < min_col_[dst])
 			min_col_[dst] = min_col_[src];
+
+		int drain_worst = std::min<int>(b_drain_percent*((signed) actual_damage)/100+b_drain_constant, b_drain_constant);
+		if(drain_worst < 0) {
+			if((unsigned) -drain_worst >= min_col_[dst])
+				min_col_[dst] = 0;
+			else
+				min_col_[dst] += drain_worst;
+		}
 	}
 }
 
@@ -811,13 +863,13 @@ void combatant::complex_fight(combatant &opp, unsigned rounds, bool levelup_cons
 			if (i < hit_chances_.size()) {
 				debug(("A strikes\n"));
 				m.receive_blow_b(a_damage, a_slow_damage, hit_chances_[i],
-								 u_.slows && !opp.u_.is_slowed, u_.drains);
+								 u_.slows && !opp.u_.is_slowed, u_.drain_constant, u_.drain_percent);
 				m.dump();
 			}
 			if (i < opp.hit_chances_.size()) {
 				debug(("B strikes\n"));
 				m.receive_blow_a(b_damage, b_slow_damage, opp.hit_chances_[i],
-								 opp.u_.slows && !u_.is_slowed, opp.u_.drains);
+								 opp.u_.slows && !u_.is_slowed, opp.u_.drain_constant, opp.u_.drain_percent);
 				m.dump();
 			}
 		}

data/scenario-test.cfg
@@ -195,7 +195,165 @@ Xu          , Xu          , Qxu         , Qxu         , Ql          , Ql        
                     [/effect]
                     [effect]
                         apply_to=attack
+                        name=chill wave
+                        increase_attacks=12
+                        [set_specials]
+                            mode=append
+                            [heal_on_hit]
+                                id=sacrifice
+                                name="Sacrifice"
+                                description="Sacrifice:
+This unit sacrifices its own life in order to make more attacks."
+                                value=-5
+                            [/heal_on_hit]
+                        [/set_specials]
+                    [/effect]
+                    [effect]
+                        apply_to=attack
+                        name=shadow wave
                         increase_attacks=18
+                        [set_specials]
+                            mode=append
+                            [heal_on_hit]
+                                id=sacrifice
+                                name="Greater Sacrifice"
+                                description="Greater Sacrifice:
+This unit sacrifices its own life in order to make more attacks."
+                                value=-6
+                            [/heal_on_hit]
+                        [/set_specials]
+                    [/effect]
+                [/object]
+            [/modifications]
+        [/unit]
+        [unit]
+            # Drain tester
+            x,y=20,7
+            type="Elvish Champion"
+            generate_name=yes
+            [modifications]
+                [object]
+                    silent=yes
+                    [effect]
+                        apply_to=remove_attacks
+                    [/effect]
+                    [effect]
+                        apply_to=new_attack
+                        name=sword
+                        description=_"sword"
+                        icon=attacks/greatsword-elven.png
+                        type=blade
+                        [specials]
+                            [drains]
+                                id=life_vortex
+                                name= _ "Life Vortex"
+                                description= _ "Life Vortex:
+Each successful strike in a battle involving this attack causes the attacker to gain the health that his attack removed from the target.  This has no effect against nonliving targets."
+                                value=0
+                                add=100
+                                apply_to=both
+                            [/drains]
+                            [heal_on_hit]
+                                id=entropy
+                                name= _ "Entropy"
+                                description= _ "Entropy:
+Each successful strike in a battle involving this attack causes the attacker to lose 4 HP.  This has no effect against nonliving targets."
+                                [filter_opponent]
+                                    [not]
+                                        [filter_wml]
+                                            [status]
+                                                not_living="yes"
+                                            [/status]
+                                        [/filter_wml]
+                                    [/not]
+                                [/filter_opponent]
+                                sub=4
+                                apply_to=both
+                            [/heal_on_hit]
+                        [/specials]
+                        range=melee
+                        damage=9
+                        number=5
+                    [/effect]
+                    [effect]
+                        apply_to=new_attack
+                        name=bow
+                        description=_"bow"
+                        icon=attacks/bow-elven.png
+                        type=pierce
+                        [specials]
+                            [drains]
+                                id=empathic_weapon
+                                name= _ "Empathic Weapon"
+                                description= _ "Empathic Weapon:
+All damage dealt by this attack to a living target is dealt to the user as well"
+                                multiply=-2
+                            [/drains]
+                            [heal_on_hit]
+                                id=rejuvenating_weapon
+                                name= _ "Rejuvenating Weapon (10)"
+                                description= _ "Rejuvenating Weapon (10):
+Each successful strike with this weapon heals the user for 10 HP"
+                                value=10
+                            [/heal_on_hit]
+                        [/specials]
+                        range=ranged
+                        damage=9
+                        number=3
+                    [/effect]
+                    [effect]
+                        apply_to=new_attack
+                        name=longbow
+                        description=_"longbow"
+                        icon=attacks/bow-elven-magic.png
+                        type=pierce
+                        [specials]
+                            {WEAPON_SPECIAL_MARKSMAN}
+                            [drains]
+                                id=drains
+                                name= _ "drains 20%"
+                                description= _ "Drain 20%:
+This unit drains health from living units, healing itself for 20% of the amount of damage it deals (rounded down)."
+                                value=20
+                            [/drains]
+                            [heal_on_hit]
+                                id=drains
+                                name= _ "sacrifice 5"
+                                description= _ "Sacrifice 5:
+Each successful attack costs this unit 5 HP."
+                                sub=5
+                            [/heal_on_hit]
+                        [/specials]
+                        range=ranged
+                        damage=10
+                        number=5
+                    [/effect]
+                    [effect]
+                        apply_to=new_attack
+                        name=life bomb
+                        description=_"life bomb"
+                        type=impact
+                        [specials]
+                            [heal_on_hit]
+                                id=drains
+                                name= _ "life copy 20"
+                                description= _ "Life copy 20:
+When this attack hits a living unit, the user is healed for 20 HP."
+                                [filter_opponent]
+                                    [not]
+                                        [filter_wml]
+                                            [status]
+                                                not_living="yes"
+                                            [/status]
+                                        [/filter_wml]
+                                    [/not]
+                                [/filter_opponent]
+                                value=20
+                            [/heal_on_hit]
+                        [/specials]
+                        damage=3
+                        number=2
+                        range=ranged
                     [/effect]
                 [/object]
             [/modifications]
@@ -366,6 +524,61 @@ Xu          , Xu          , Qxu         , Qxu         , Ql          , Ql        
                     [effect]
                         apply_to=movement
                         set=0
+                    [/effect]
+                [/object]
+            [/modifications]
+        [/unit]
+        [unit]
+            # Drain tester (for undead and default drain)
+            x,y=21,7
+            type="Lich"
+            generate_name=yes
+        [/unit]
+        [unit]
+            # Drain tester
+            x,y=21,8
+            type="Elvish Sharpshooter"
+            generate_name=yes
+            [modifications]
+                [object]
+                    silent=yes
+                    [effect]
+                        apply_to=attack
+                        name=sword
+                        [set_specials]
+                            mode=append
+                            [heal_on_hit]
+                                id=fiery
+                                name= _ "fiery 4"
+                                name_inactive= _ "fiery 4"
+                                description= _ "Fiery 4:
+A successful hit against this unit costs the opponent 4 HP."
+                                description_inactive= _ "Fiery 4:
+A successful hit against this unit costs the opponent 4 HP."
+                                sub=4
+                                apply_to=opponent
+                            [/heal_on_hit]
+                        [/set_specials]
+                    [/effect]
+                    [effect]
+                        apply_to=new_attack
+                        name=lifebow
+                        description=_"lifebow"
+                        icon=attacks/bow-elven-magic.png
+                        type=pierce
+                        [specials]
+                            {WEAPON_SPECIAL_MARKSMAN}
+                            [drains]
+                                id=drains
+                                name= _ "drains 100%"
+                                description= _ "Drain 100%:
+This unit drains health from living units, healing itself for 100% of the amount of damage it deals (rounded down)."
+                                value=100
+                            [/drains]
+                        [/specials]
+                        range=ranged
+                        damage=4
+                        number=5
                     [/effect]
                 [/object]
             [/modifications]

commit 2bb60fbfe90033c97c7912fa374fd2d71e4143c7

Fix some issues introduced in r53470.
The author of patch #3083 posted a followup post fixing:
- Some unwanted changes from unsigned to signed, forcing extra casts.
- C-style casts to C++-style casts.
- Reverted code that allowed for both units to die (which is not allowed).


git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@53487 75b3775d-3801-0410-9e12-e2e43121521d

src/attack_prediction.cpp
@@ -105,8 +105,8 @@ private:
 
 	// Move this much from src to dst.  Returns true if anything transferred.
 	void xfer(unsigned dst_plane, unsigned src_plane,
-			  int row_dst, int col_dst,
-			  int row_src, int col_src,
+			  unsigned row_dst, unsigned col_dst,
+			  unsigned row_src, unsigned col_src,
 			  double prob);
 
 	// Shift columns on this plane (b taking damage).  Returns min col.
@@ -256,8 +256,8 @@ void prob_matrix::dump() const
 
 // xfer, shift_cols and shift_rows use up most of our time.  Careful!
 void prob_matrix::xfer(unsigned dst_plane, unsigned src_plane,
-					   int row_dst, int col_dst,
-					   int row_src, int col_src,
+					   unsigned row_dst, unsigned col_dst,
+					   unsigned row_src, unsigned col_src,
 					   double prob)
 {
 	double &src = val(src_plane, row_src, col_src);
@@ -266,14 +266,10 @@ void prob_matrix::xfer(unsigned dst_plane, unsigned src_plane,
 		src -= diff;
 
 		// This is here for drain.
-		if (col_dst >= (signed) cols_)
-			col_dst = (signed) cols_ - 1;
-		else if (col_dst < 0)
-			col_dst = 0;
-		if (row_dst >= (signed) rows_)
-			row_dst = (signed) rows_ - 1;
-		else if (row_dst < 0)
-			row_dst = 0;
+		if (col_dst >= cols_)
+			col_dst = cols_ - 1;
+		if (row_dst >= rows_)
+			row_dst = rows_ - 1;
 
 		val(dst_plane, row_dst, col_dst) += diff;
 
@@ -294,8 +290,8 @@ void prob_matrix::xfer(unsigned dst_plane, unsigned src_plane,
 void prob_matrix::shift_cols(unsigned dst, unsigned src,
 							 unsigned damage, double prob, int drain_constant, int drain_percent)
 {
-	int row, col;
-	int drainmax = (drain_percent*((signed) damage)/100+drain_constant);
+	unsigned int row, col;
+	int drainmax = (drain_percent*(static_cast<signed>(damage))/100+drain_constant);
 
 	if(drain_constant || drain_percent) {
 		debug(("Drains %i (%i%% of %i plus %i)\n", drainmax, drain_percent, damage, drain_constant));
@@ -305,23 +301,27 @@ void prob_matrix::shift_cols(unsigned dst, unsigned src,
 		damage = cols_ - 1;
 
 	// Killing blows can have strange drain amounts, so handle them first
-	for (row = min_row_[src] + 1; row < (signed) rows_; row++) {
+	for (row = min_row_[src] + 1; row < rows_; row++) {
 		// These are all going to die (move to col 0).
-		for (col = 1; (unsigned) col <= damage; ++col)
-			xfer(dst, src, std::max(row+(drain_percent*col/100+drain_constant), 1), 0, row, col, prob);
+		for (col = 1; col <= damage; ++col) {
+			int newrow = std::max(static_cast<signed>(row)+(drain_percent*static_cast<signed>(col)/100+drain_constant), 1);
+			xfer(dst, src, static_cast<unsigned>(newrow), 0, row, col, prob);
+		}
 	}
 
 	// Loop downwards if we drain positive, but upwards if we drain negative,
 	// so we write behind us (for when src == dst).
 	if(drainmax > 0) {
-		for (row = rows_ - 1; row > (signed) min_row_[src]; row--) {
-			for (col = damage+1; (unsigned) col < cols_; ++col)
-				xfer(dst, src, std::max(row+drainmax, 1), col - damage, row, col, prob);
+		for (row = rows_ - 1; row > min_row_[src]; row--) {
+			int newrow = std::max(static_cast<signed>(row)+drainmax, 1);
+			for (col = damage+1; col < cols_; ++col)
+				xfer(dst, src, static_cast<unsigned>(newrow), col - damage, row, col, prob);
 		}
 	} else {
-		for (row = min_row_[src] + 1; (unsigned) row < rows_; row++) {
-			for (col = damage+1; (unsigned) col < cols_; ++col)
-				xfer(dst, src, std::max(row+drainmax, 1), col - damage, row, col, prob);
+		for (row = min_row_[src] + 1; row < rows_; row++) {
+			int newrow = std::max(static_cast<signed>(row)+drainmax, 1);
+			for (col = damage+1; col < cols_; ++col)
+				xfer(dst, src, static_cast<unsigned>(newrow), col - damage, row, col, prob);
 		}
 	}
 }
@@ -329,8 +329,8 @@ void prob_matrix::shift_cols(unsigned dst, unsigned src,
 void prob_matrix::shift_rows(unsigned dst, unsigned src,
 							 unsigned damage, double prob, int drain_constant, int drain_percent)
 {
-	int row, col;
-	int drainmax = (drain_percent*((signed) damage)/100+drain_constant);
+	unsigned int row, col;
+	int drainmax = (drain_percent*(static_cast<signed>(damage))/100+drain_constant);
 
 	if(drain_constant || drain_percent) {
 		debug(("Drains %i (%i%% of %i plus %i)\n", drainmax, drain_percent, damage, drain_constant));
@@ -340,23 +340,27 @@ void prob_matrix::shift_rows(unsigned dst, unsigned src,
 		damage = rows_ - 1;
 
 	// Killing blows can have strange drain amounts, so handle them first
-	for (col = min_col_[src] + 1; (unsigned) col < cols_; col++) {
+	for (col = min_col_[src] + 1; col < cols_; col++) {
 		// These are all going to die (move to row 0).
-		for (row = 1; row <= (signed) damage; ++row)
-			xfer(dst, src, 0, std::max(col+(drain_percent*row/100+drain_constant), 1), row, col, prob);
+		for (row = 1; row <= damage; ++row) {
+			int newcol = std::max(static_cast<signed>(col)+(drain_percent*static_cast<signed>(row)/100+drain_constant), 1);
+			xfer(dst, src, 0, static_cast<unsigned>(newcol), row, col, prob);
+		}
 	}
 
 	// Loop downwards if we drain positive, but upwards if we drain negative,
 	// so we write behind us (for when src == dst).
 	if(drainmax > 0) {
-		for (col = cols_ - 1; col > (signed) min_col_[src]; col--) {
-			for (row = damage+1; (unsigned) row < rows_; ++row)
-				xfer(dst, src, row - damage, std::max(col+drainmax, 1), row, col, prob);
+		for (col = cols_ - 1; col > min_col_[src]; col--) {
+			int newcol = std::max(static_cast<signed>(col)+drainmax, 1);
+			for (row = damage+1; row < rows_; ++row)
+				xfer(dst, src, row - damage, static_cast<unsigned>(newcol), row, col, prob);
 		}
 	} else {
-		for (col = min_col_[src] + 1; (unsigned) col < cols_; col++) {
-			for (row = damage+1; (unsigned) row < rows_; ++row)
-				xfer(dst, src, row - damage, std::max(col+drainmax, 1), row, col, prob);
+		for (col = min_col_[src] + 1; col < cols_; col++) {
+			int newcol = std::max(static_cast<signed>(col)+drainmax, 1);
+			for (row = damage+1; row < rows_; ++row)
+				xfer(dst, src, row - damage, static_cast<unsigned>(newcol), row, col, prob);
 		}
 	}
 }
@@ -395,12 +399,13 @@ void prob_matrix::receive_blow_b(unsigned damage, unsigned slow_damage, double h
 		if (min_row_[src] < min_row_[dst])
 			min_row_[dst] = min_row_[src];
 
-		int drain_worst = std::min<int>(a_drain_percent*((signed) actual_damage)/100+a_drain_constant, a_drain_constant);
+		int drain_worst = std::min<int>(a_drain_percent*(static_cast<signed>(actual_damage))/100+a_drain_constant, a_drain_constant);
 		if(drain_worst < 0) {
-			if((unsigned) -drain_worst >= min_row_[dst])
+			unsigned int max_drain_damage = static_cast<unsigned>(-drain_worst);
+			if(max_drain_damage >= min_row_[dst])
 				min_row_[dst] = 0;
 			else
-				min_row_[dst] += drain_worst;
+				min_row_[dst] -= max_drain_damage;
 		}
 	}
 }
@@ -486,10 +491,10 @@ double prob_matrix::dead_prob() const
 	for (p = 0; p < 4; ++p) {
 		if (!plane_[p])
 			continue;
-		// Don't count 0,0 twice
+		// We might count 0,0 twice, but that is always 0 anyway.
 		for (row = min_row_[p]; row < rows_; ++row)
 			prob += val(p, row, 0);
-		for (col = min_col_[p]?min_col_[p]:1; col < cols_; ++col)
+		for (col = min_col_[p]; col < cols_; ++col)
 			prob += val(p, 0, col);
 	}
 	return prob;
@@ -529,12 +534,13 @@ void prob_matrix::receive_blow_a(unsigned damage, unsigned slow_damage, double h
 		if (min_col_[src] < min_col_[dst])
 			min_col_[dst] = min_col_[src];
 
-		int drain_worst = std::min<int>(b_drain_percent*((signed) actual_damage)/100+b_drain_constant, b_drain_constant);
+		int drain_worst = std::min<int>(b_drain_percent*(static_cast<signed>(actual_damage))/100+b_drain_constant, b_drain_constant);
 		if(drain_worst < 0) {
-			if((unsigned) -drain_worst >= min_col_[dst])
+			unsigned int max_drain_damage = static_cast<unsigned>(-drain_worst);
+			if(max_drain_damage >= min_col_[dst])
 				min_col_[dst] = 0;
 			else
-				min_col_[dst] += drain_worst;
+				min_col_[dst] -= max_drain_damage;
 		}
 	}
 }